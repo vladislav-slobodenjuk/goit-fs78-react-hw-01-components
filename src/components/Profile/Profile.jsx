c370fc49e5a5b1fb951a475591bafedcc83ef46f
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-12 22:31:42 +0300

    Statistics

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,30 @@
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   const { followers, views, likes } = stats;
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
-      </div>
+    <section>
+      <div className="profile">
+        <div className="description">
+          <img src={avatar} alt="User avatar" className="avatar" />
+          <p className="name">{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity">{followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity">{views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity">{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,14 @@
-export const Statistics = ({ title }) => (
-  <section class="statistics">
-    {title && <h2 class="title">{title}</h2>}
+export const Statistics = ({ title, stats }) => (
+  <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
 
-    <ul class="stat-list">
-      <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
-      </li>
-      <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
-      </li>
+    <ul className="stat-list">
+      {stats.map(({ id, label, percentage }) => (
+        <li key={id} className="item">
+          <span className="label">{label}</span>
+          <span className="percentage">{percentage}</span>
+        </li>
+      ))}
     </ul>
   </section>
 );
