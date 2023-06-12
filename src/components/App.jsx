5d4c05494f6327d7fd20b5a8a9857359e1faeadc
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-12 23:45:26 +0300

    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from '../data/user.json';
 import statistics from '../data/statistics.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -26,10 +28,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={statistics} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
